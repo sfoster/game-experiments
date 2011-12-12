d85a0e25b3de6e9737563f5ecfe1c532c8b01ce5
sfoster <sam@sam-i-am.com>
2011-12-12 12:06:58 +0000

    Using explicitly versioned crafty.js. Added profile/profileEnd to both the tutorial and my code to try and get insight into out of control script execution

--- tutorial.js
+++ tutorial.js
@@ -1,4 +1,25 @@
 window.onload = function() {
+  console.profile();
+  setTimeout(function(){
+    console.profileEnd();
+    Crafty.stop();
+  }, 2000);
+
+	//start crafty
+	Crafty.init(400, 320);
+	Crafty.canvas();
+	
+	//turn the sprite map into usable components
+	Crafty.sprite(16, "sprite.png", {
+		grass1: [0,0],
+		grass2: [1,0],
+		grass3: [2,0],
+		grass4: [3,0],
+		flower: [0,1],
+		bush1: [0,2],
+		bush2: [1,2],
+		player: [0,3]
+	});
 	
 	//method to randomy generate the map
 	function generateWorld() {
